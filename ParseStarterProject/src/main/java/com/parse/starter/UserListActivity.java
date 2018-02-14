c5e4d82bd0c8e7aad97359e8ca3886d241b5b8b8
Sandeep <sandeeptile@gmail.com>
2018-02-15 05:18:58 +0530

    viewing users feeds

--- ParseStarterProject/src/main/java/com/parse/starter/UserListActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/UserListActivity.java
@@ -14,6 +14,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -92,6 +94,13 @@ public class UserListActivity extends AppCompatActivity {
 
             }
 
+        }else if(item.getItemId()==R.id.logout){
+
+            ParseUser.logOut();
+
+            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
@@ -156,9 +165,21 @@ public class UserListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
 
+        setTitle("User Feed");
+
         final ArrayList<String> usernames = new ArrayList<String>();
 
         final ListView userListView = (ListView) findViewById(R.id.userListView);
+
+        userListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intent=new Intent(getApplicationContext(),UserFeedActivity.class);
+                intent.putExtra("username",usernames.get(i));
+                startActivity(intent);
+            }
+        });
 
         final ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, usernames);
 
